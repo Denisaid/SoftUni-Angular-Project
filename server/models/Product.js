b9a36f2283be59971ad7756449d58ca16136c55b
Deniz Said <138148614+Denisaid@users.noreply.github.com>
2024-03-29 19:59:21 +0200

    Added Product Model

---
+++ server/models/Product.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Product name is required'],
+        maxlength: [100, 'Product must be a maximum of one hundred characters long']
+    },
+    description: {
+        type: String,
+        required: [true, 'Description is required'],
+        maxlength: [10, 'Product must be a maximum of ten characters long']
+    },
+    price: {
+        type: Number,
+        required: [true, 'Price is required'],
+        validate: {
+            validator: (value) => value >= 0,
+            message: 'Price must be a positive number'
+        }
+    },
+    material: {
+        type: String,
+        required: [true, 'Material is required'],
+        maxlength: [20, 'Material must be a maximum of twenty characters long']
+    },
+    image: {
+        type: String,
+        required: [true, 'Image is required'],
+        match: [/^https?:\/\/[^ ]+$/gi, 'Image URL must start with http:// or https://'],
+    },
+    storeId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Store',
+        required: true
+    }
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = { Product };
